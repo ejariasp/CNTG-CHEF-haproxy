874e5f7ce9af08eae097e3a444d1b96c4839692e
ejariasp <ejariasp@yahoo.es>
2017-06-14 11:43:45 +0200

    Modificadas receta y plantilla y subida de versión

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,8 +6,14 @@
 
 package "haproxy"
 
+allwebservers = search('node', 'recipes:apache\:\:default')
+
 template "/etc/haproxy/haproxy.cfg" do
   source "haproxy.cfg.erb"
+  variables(
+    :webservers => allwebservers
+  )
+  notifies :restart, "service[haproxy]"
 end
 
 service "haproxy" do
