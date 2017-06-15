4d2082599ef3ad2b1eac209122dcb5651f9e8aaa
ejariasp <ejariasp@yahoo.es>
2017-06-15 10:55:52 +0200

    Añadido obtención de puerto destino mediante valor de atributo.
    Modificación de README.md, metadata.rb y Berksfile para añadir la dependencia.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # haproxy
 
-TODO: Enter the cookbook description here.
+Realiza la instalaci√n y configuraci√n del balanceador.
 
+versi√n (0.2.0)
+---
+Toma el dato del puerto a redirigir del valor de un atributo `default["apache"]["port"]` en el fichero `attributes/default.rb`. Por defecto es el puerto 8080. Depende de recetario apache, v0.3.0.
+
+
+versi√≥n (0.1.0)
+---
+Se automatiza la configuraci√≥n de los servidores web mediante plantilla `templates/haproxy.cfg.erb`
--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name 'haproxy'
-maintainer 'The Authors'
+maintainer 'Emilio'
 maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures haproxy'
-long_description 'Installs/Configures haproxy'
-version '0.1.1'
+long_description 'Installs/Configures haproxy, redirecting data to port [apache][port], by default 8080'
+version '0.2.0'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
@@ -13,3 +13,4 @@ version '0.1.1'
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Source` link
 # source_url 'https://github.com/<insert_org_here>/haproxy' if respond_to?(:source_url)
+depends "apache", ">= 0.3.0"
