b4aa25ce873e1a3a3eaa0dfcfab6cbdee2b33697
ejariasp <ejariasp@yahoo.es>
2017-06-15 11:17:37 +0200

    modificado operador de compartiva de versiones en metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -13,4 +13,4 @@ version '0.2.0'
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Source` link
 # source_url 'https://github.com/<insert_org_here>/haproxy' if respond_to?(:source_url)
-depends "apache", ">= 0.3.0"
+depends "apache", "~> 0.3.0"
