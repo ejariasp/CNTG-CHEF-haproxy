386b095cdbc3fdd121b3213e3a08cbaf7c10d1a1
ejariasp <ejariasp@yahoo.es>
2017-06-16 13:51:30 +0200

    viernes

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures haproxy'
 long_description 'Installs/Configures haproxy, redirecting data to port [apache][port], by default 8181, aplying configuration by role'
-version '0.3.0'
+version '0.3.4'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
@@ -13,4 +13,4 @@ version '0.3.0'
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Source` link
 # source_url 'https://github.com/<insert_org_here>/haproxy' if respond_to?(:source_url)
-depends "apache", "~> 0.3.0"
+depends "apache", "~> 0.3.1"
